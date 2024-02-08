48371daec4e841606483a7febcbc1184f47d2615
FASALGAFOOR <fasalgafoor2080@gmail.com>
2024-02-08 09:59:11 +0530

    forgot pass bug fix

--- src/Components/Usercomponents/Userpassword/Newpass.jsx
+++ src/Components/Usercomponents/Userpassword/Newpass.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { ToastContainer, toast } from "react-toastify";
 import { Newpassword } from '../../../Api/Userapi';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Newpass() {
     const navigate = useNavigate()
@@ -11,75 +11,71 @@ function Newpass() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (password === "" && confirmPassword === "") {
-            toast.error('fields empty')
-        }
-        if (password != confirmPassword) {
-            toast.error("password and confirmpassword doesn't match")
-        }
-
-        const Req = await Newpassword({password, confirmPassword})
-      
-        try {
-            if(Req.data.userdata){
-                toast.success(Req.data.message)
-                navigate('/login')
-
-            }else{
-                toast.error("error got")
+        if (password === "" || confirmPassword === "") {
+            toast.error('fields empty');
+        } else if (password !== confirmPassword) {
+            toast.error("password and confirmPassword don't match");
+        } else {
+            try {
+                const Req = await Newpassword({ password, confirmPassword });
+                console.log(Req, "oo");
+                if (Req.data.success === true) {
+                    console.log("lklkl");
+                    toast.success(Req.data.message);
+                    navigate('/login');
+                }
+            } catch (error) {
+                console.log(error);
             }
-      
-        } catch (error) {
-            console.log(error);
         }
+    };
+    
+        
+        return (
+            <div className='bg-pink-50 min-h-screen flex items-center justify-center'>
+                <div className="w-full sm:w-[35%] mx-10 bg-gradient-to-r from-[#bfd8e1] to-[#ee8e8e] p-8 rounded-xl shadow shadow-slate-300">
+                    <h3 ><strong>Enter new password </strong></h3>
 
+                    <form onSubmit={handleSubmit} className="mt-8">
+                        <div className="mb-4">
+                            <label htmlFor="password" className="block mb-2 font-medium text-lg text-gray-800">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="w-full px-4 py-2 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
 
-    }
-    return (
-        <div className='bg-pink-50 min-h-screen flex items-center justify-center'>
-            <div className="w-full sm:w-[35%] mx-10 bg-gradient-to-r from-[#bfd8e1] to-[#ee8e8e] p-8 rounded-xl shadow shadow-slate-300">
-                <h3 ><strong>Enter new password </strong></h3>
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="confirmPassword" className="block mb-2 font-medium text-lg text-gray-800">
+                                Confirm Password
+                            </label>
+                            <input
+                                type="password"
+                                id="confirmPassword"
+                                className="w-full px-4 py-2 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
+                                placeholder="Confirm your password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
 
-                <form onSubmit={handleSubmit} className="mt-8">
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block mb-2 font-medium text-lg text-gray-800">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="w-full px-4 py-2 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
-                            placeholder="Enter your password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="confirmPassword" className="block mb-2 font-medium text-lg text-gray-800">
-                            Confirm Password
-                        </label>
-                        <input
-                            type="password"
-                            id="confirmPassword"
-                            className="w-full px-4 py-2 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
-                            placeholder="Confirm your password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full px-4 py-2 text-lg font-semibold text-white bg-[#dc5151] hover:bg-pink-400 focus:outline-none rounded-xl"
-                    >
-                        Submit
-                    </button>
-                </form>
-                <ToastContainer />
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full px-4 py-2 text-lg font-semibold text-white bg-[#dc5151] hover:bg-pink-400 focus:outline-none rounded-xl"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                    <ToastContainer />
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
-export default Newpass
+    export default Newpass
