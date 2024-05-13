f3c1010f474e5251c7a01dc303df0a8705751a0d
FASALGAFOOR <fasalgafoor2080@gmail.com>
2024-05-13 16:55:43 +0530

    impoert sjdfhbkshdb

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './Redux/store.jsx';
-import { store } from './Redux/store.jsx';
+import { store ,persistor} from './Redux/store.jsx';
 
 const clientId = import.meta.env.VITE_CLIENT_ID
 ReactDOM.createRoot(document.getElementById('root')).render(
