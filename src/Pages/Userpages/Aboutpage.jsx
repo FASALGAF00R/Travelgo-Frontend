e5113953e63886517255c40e61bb73808ca70986
FASALGAFOOR <fasalgafoor2080@gmail.com>
2025-02-18 16:53:07 +0530

    about page sfgsdfg

--- src/Pages/Userpages/Aboutpage.jsx
+++ src/Pages/Userpages/Aboutpage.jsx
@@ -1,68 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { FetchCategory, fetchpackagesabout, fetchrating, } from '../../Api/Userapi';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
 
 function Aboutpage() {
-
-
-    const [packages, setpackages] = useState([])
-    const [categories, setCategories] = useState([]);
-    const [review, setreview] = useState([])
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetchpackagesabout();
-                setpackages(response.data.fullpackage)
-
-                // Fetch categories
-                const categoriesResponse = await FetchCategory();
-                setCategories(categoriesResponse.data.packagescat);
-
-
-                const Reviews = await fetchrating()
-                if (Reviews?.data?.packageRatings) {
-                    setreview(Reviews.data.packageRatings);
-                }
-
-
-
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
-        fetchData();
-
-    }, [review])
-
-
-    let averageRating
-
     return (
         <>
             <div className="w-screen h-screen overflow-x-hidden ">
-                <div className="flex flex-row w-full h-90">
-                    <div className="w-1/2 sm:content lg:w-1/2 mr-4 p-7 pl-7 mt-16 ml-12 h-[100%] rounded-2xl bg-pink-200 shadow shadow-violet-900">
-                        <h3 className="text-[#ffffff] ml-2 font-prompt-semibold text-2xl">
-                            About Us 
+                <div className="flex flex-row w-full h-80">
+                      <div className="w-1/2 sm:content lg:w-1/2 mr-4 p-7 pl-7 mt-16 ml-12 h-[100%] rounded-2xl bg-pink-200 shadow shadow-violet-900">
+                        <h3 className="text-[#ffffff] ml-2 font-prompt-semibold">
+                            ttttttt
                         </h3>
-                        <h1 className="text-sm sm:text-4xl lg:text-5xl font-serif">
-                            <span className="text-white">
-                                <span className="text-black">Welcome to Travelgooo,</span> your trusted platform for discovering and booking the best travel packages in your state!
+                        <h1 className="text-2xl sm:text-4xl lg:text-6xl font-prompt-lrlight">
+                            <span className="text-[#af4677]">
+                                <span className="text-black">ggfg</span>gg
                             </span>
-        
+                            <br />
+                            <span className="text-[#ffffff] block mt-7 ">
+                                lllll
+                            </span>
+                            <span className="text-[#000000] block mt-4 ">uuuu</span>
                         </h1>
                     </div>
 
                     <div className="w-1/2 sm:content lg:w-1/2 mr-4 p-7 pl-7 mt-16 ml-12 h-[100%] rounded-2xl bg-pink-200 shadow shadow-violet-900">
-                        <h1 className="text-sm sm:text-4xl lg:text-4xl font-serif">
-                            <span className="text-white">
-                                <span className="text-black">We believe that travel should be easy, affordable, and tailored to your preferences. Whether you’re looking for a weekend getaway, an adventure trip, or a luxury vacation,
-                                </span> we bring you carefully curated travel packages designed to suit every traveler’s needs.
+                        <h3 className="text-[#ffffff] ml-2 font-prompt-semibold">
+                            ttttttt
+                        </h3>
+                        <h1 className="text-2xl sm:text-4xl lg:text-6xl font-prompt-lrlight">
+                            <span className="text-[#af4677]">
+                                <span className="text-black">ggfg</span>gg
                             </span>
-
+                            <br />
+                            <span className="text-[#ffffff] block mt-7 ">
+                                lllll
+                            </span>
+                            <span className="text-[#000000] block mt-4 ">uuuu</span>
                         </h1>
                     </div>
 
@@ -70,38 +41,34 @@ function Aboutpage() {
 
 
                 </div>
-
                 <div className="flex flex-col items-center justify-center p-28">
                     <h1 className="text-center mb-10 mt-2 font-prompt text-3xl">
                         Explore packages
                     </h1>
                     <div className="flex flex-col sm:flex lg:flex-row gap-8">
-                        {/* Ensure this is a flex-row container */}
-                        <div className="flex flex-row justify-center gap-10 flex-wrap">
-                            {packages.map((pk) => (
-                                <div key={pk._id} className="w-64 h-80 shadow  rounded-2xl shadow-lg shadow-gray-400 border-2 border-gray-400 rounded-lg overflow-hidden card transform transition-transform duration-200 hover:scale-105 hover:shadow-md">
-                                    <img
-                                        src={pk.Image[0]}
-                                        alt={pk.Destrictname}
-                                        className="object-cover w-full h-40"
-                                    />
-                                    <h1 className="text-center text-2xl font-semibold font-prompt">
-                                        {pk.category}
-                                    </h1>
-                                    <div className="flex flex-row p-4">
-                                        <p className="ml-1 mb-2 text-base">{pk.Destrictname}</p>
-                                    </div>
-                                    <div key={pk.packageId} className="p-4">
-                                        <p className="mr-1 mt-4 mb-2 text-yellow-800">
-                                            <FontAwesomeIcon icon={faStar} /> Ratings: {pk.averageRating || "0"}
-                                        </p>
-                                    </div>
+                
+
+                        <div className="flex flex-wrap justify-center gap-10 ">
+                            <div className="w-64 h-80  shadow bg-pink-200 rounded-2xl">
+                                <img
+                                    className="p-3 pr-4 h-[75%] ml-2 justify-center items-center"
+                                    //   src={photoshop}
+                                    alt=""
+                                />
+                                <h1 className="text-center text-2xl font-prompt-semibold font-prompt">
+                                    {/* {category[2]} */}
+                                </h1>
+                                <div className="flex flex-row p-4">
+                                    {/* <img className="w-4 h-4 mt-1" src={course} alt="" /> */}
+                                    <p className="ml-1 mb-2 text-base">jjjj</p>
+                                    {/* <img className="w-3 h-4 mt-1 ml-8" src={Instructors} alt="" /> */}
+                                    <p className=" mb-2 text-base">jjj</p>
                                 </div>
-                            ))}
+                            </div>
                         </div>
+
                     </div>
                 </div>
-
             </div>
         </>
     )
